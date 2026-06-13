3ce0b626b2301786791e3d03f81ec7586d7b4565
junze0925 <2393646957@qq.com>
2026-06-13 19:51:14 +0800

    Update API endpoints to use relative paths
    高德天气更换成代理

--- src/api/index.js
+++ src/api/index.js
@@ -55,14 +55,13 @@ export const getHitokoto = async () => {
 
 // 获取高德地理位置信息
 export const getAdcode = async (key) => {
-  const res = await fetch(`https://restapi.amap.com/v3/ip?key=${key}`);
+  const res = await fetch(`/amap/v3/ip?key=${key}`);
   return await res.json();
 };
 
 // 获取高德地理天气信息
 export const getWeather = async (key, city) => {
-  const res = await fetch(
-    `https://restapi.amap.com/v3/weather/weatherInfo?key=${key}&city=${city}`);
+  const res = await fetch(`/amap/v3/weather/weatherInfo?key=${key}&city=${city}`);
   return await res.json();
 };
 
