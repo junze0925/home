ceaeec53e39b90544352b91039786fa7d22e5614
junze0925 <2393646957@qq.com>
2026-06-16 18:29:17 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -110,15 +110,8 @@ onMounted(() => {
   const styleTitle2 = "font-size:12px;color: rgb(244,167,89);";
   const styleContent = "color: rgb(30,152,255);";
   const title1 = "君泽の主页";
-  const title2 = `const title2 = `
-            ██     ██        ███████      ███           ███    ██████████
-            ██     ██      ██       ██    ██ ██       ██ ██    ██
-            ██     ██      ██       ██    ██  ██     ██  ██    ██
-            █████████      ██       ██    ██   ██   ██   ██    █████████
-            ██     ██      ██       ██    ██     ███     ██    ██
-            ██     ██      ██       ██    ██      █      ██    ██
-            ██     ██       ████████      ██             ██    ███████████
-                                   
+  const title2 = `const title2 = ` 
+    MY HOME
 `;
   const content = `\n\n版本: ${config.version}\n主页: ${config.home}\nGithub: ${config.github}`;
   console.info(`%c${title1} %c${title2} %c${content}`, styleTitle1, styleTitle2, styleContent);
